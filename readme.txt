8c9fb57e71420870d81641f65feb0e7a7d968bf7
Phi Huynh <phihuynh@Phis-MacBook-Pro.local>
2020-07-27 00:28:11 -0600

    first commit

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Tutorial: React JS To-Do App - Get Started with React
+
+Author: CodAffection
+
+Link: https://www.youtube.com/watch?v=nNevGqGjWiQ
---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+
+  state = {
+    todoList: []
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="jumbotron jumbotron-fluid py-2">
+          <div className="container">
+            <h1 className="display-4">Todos Apps</h1>
+          </div>
+        </div>
+        <form className="mb-3" onSubmit={this.handleSubmit}>
+          <div className="input-group">
+            <input type="text" name="todoTask" className="form-control" placeholder="Please enter your task" autoComplete="off"/>
+            <div className="input-group-append">
+              <button type="submit" className="btn btn-outline-success">Add</button>
+            </div>
+          </div>
+        </form>
+
+        <ul className="list-group">
+          {
+            this.state.todoList.map(
+              (item, index) => {
+                return <li className="list-group-item" key={index}>
+                  { item }
+                  <button className="btn btn-sm btn-outline-danger float-right" onClick={(event) => {this.deleteTodoTask(event, index)}}>delete</button>
+                </li>
+              }
+            )
+          }
+        </ul>
+      </div>
+    );
+  }
+
+  handleSubmit = (event) => {
+    var taskDesc = event.target.elements.todoTask.value;
+  
+    if (taskDesc.length > 0){
+      this.setState({
+        todoList: [...this.state.todoList, taskDesc]
+      })
+  
+      event.target.reset();
+    }
+
+    event.preventDefault();
+  }
+
+  deleteTodoTask = (event, index) => {
+    var taskArray = [...this.state.todoList]
+    taskArray.splice(index, 1);
+    this.setState({todoList: taskArray})
+  }
+}
+
+export default App;
